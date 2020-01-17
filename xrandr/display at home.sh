016ef9b684d3685cdec619504250212d92ad503e
UzenH <uzen.huang@gmail.com>
2020-01-17 08:40:41 +0800

    removed xrandr configurations

--- xrandr/display at home.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-monitor=$(xrandr | grep DP2 | awk '{print $2}')
-
-if [ $monitor == connected ]
-then
-    xrandr --output LVDS1 --pos 0x0 --rotate normal --output DP2 --mode 2560x1080 --pos 1280x0 --rotate normal --primary
-    xrdb ~/.screenlayout/Xresources
-fi
--- xrandr/testing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-monitor=$(xrandr | grep DP2 | awk '{print $2}')
-
-if [ $monitor == connected ]
-then
-    xrandr --output LVDS1 --mode 1280x800 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --primary --mode 1280x720 --pos 1280x0 --rotate normal --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output VGA1 --off --output VIRTUAL1 --off
-fi
-

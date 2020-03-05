6dffdf34bb88407ddf3f28efcc79f6bc297b5253
CookieUzen <uzen.huang@gmail.com>
2020-03-05 18:14:03 +0800

    fixed nvim

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-cd ~/scripts
 
-case $DESKTOP_SESSION in
-	bspwm)
-	# cd golden
-	# ./run.sh &
-	# cd ..
-	# cd bspi
-	# ./bspi_listen &
-		;;
-
-	i3)
-		;;
-esac
-
-killall polybar
-killall compton 
-
-polybar "$DESKTOP_SESSION"bar &
-
-. ~/.fehbg 
+killall polybar &
+killall compton &
 
 wal -R
-
-xmodmap ~/.Xmodmap
-
-light-locker &
-compton --config /home/uzen/.config/compton/compton.conf
+polybar bspwmbar &
+compton --config /home/uzen/.config/compton/compton.conf &

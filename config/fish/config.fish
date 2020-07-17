238e5a7c7bf59a8062077efce026f9a327eb6856
Uzen Huang <uzen.huang@gmail.com>
2020-07-17 10:12:38 +0800

    created fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting ""
+
+source ~/.proxy
+export EDITOR="nvim"
+
+alias yeet='bash ~/scripts/yeet.sh'
+
+cd (cat ~/.dir)
+fish_vi_key_bindings
+
+/opt/shell-color-scripts/colorscript.sh -r 

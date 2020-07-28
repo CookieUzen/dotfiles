527636aaac441b93f74b7c4949240f52284a6196
CookieUzen <uzen.huang@gmail.com>
2020-07-28 20:04:59 +0800

    Added bc, fish

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,3 +3,4 @@
 alias zathura="detach zathura"
 alias wechat='wine "/home/uzen/.wine/drive_c/Program Files (x86)/Tencent/WeChat/WeChat.exe"'
 alias yeet='bash ~/scripts/yeet.sh'
+alias bc='bc ~/scripts/bcrc -l -q'

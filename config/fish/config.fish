1e63f998cc9a5158a5f6892178d6a5fce4733508
CookieUzen <uzen.huang@gmail.com>
2021-01-24 14:57:03 +0800

    oupdate

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,3 +9,5 @@ cd (cat ~/.dir)
 fish_vi_key_bindings
 
 /opt/shell-color-scripts/colorscript.sh -r 
+
+starship init fish | source
--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,6 +1,9 @@
 # Fish, Bash, Zsh compatiable alias file
 
 alias zathura="detach zathura"
-alias wechat='wine "/home/uzen/.wine/drive_c/Program Files (x86)/Tencent/WeChat/WeChat.exe"'
 alias yeet='bash ~/scripts/yeet.sh'
 alias bc='bc ~/scripts/bcrc -l -q'
+alias rm='rmtrash'
+alias td='todoist --color'
+alias ur='utt report'
+alias u='utt'

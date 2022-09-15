6527b047a1ab2e55c70cfe4ac439e29674b6d174
CookieUzen <uzen.huang@gmail.com>
2022-09-15 00:30:16 -0400

    migrated to lua

---
+++ config/nvim/plugin-config.lua
@@ -0,0 +1,56 @@
+-- Undotree
+vim.opt.undodir = '$HOME/.undodir'
+vim.opt.undofile = true
+
+-- Ultisnips
+vim.g.UltiSnipsExpandTrigger = "<tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<C-j>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<C-k>"
+vim.g.UltiSnipsSnippetDirectories = "snip"
+
+-- Autopair
+vim.g.AutoPairsShortcutToggle = "<>"
+vim.g.AutoPairsMapCh = "<>"
+
+-- Autosave
+vim.g.auto_save = 1
+vim.g.updatetime = 10
+vim.g.auto_save_events = {"CursorHold"}
+
+-- Quickscope
+vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+
+-- Yankstack
+vim.cmd('call yankstack#setup()')
+nmap('Y', 'y$')
+
+-- EasyAlign
+map('n', 'ga', '<Plug>(EasyAlign)')
+map('x', 'ga', '<Plug>(EasyAlign)')
+
+-- Bullets.vim
+vim.g.bullets_enabled_file_types = {'markdown', 'gitcommit', 'scratch', 'rmd'}
+
+-- Tmux Navigator
+vim.g.tmux_navigator_save_on_switch = 2
+vim.g.tmux_navigator_disable_when_zoomed = 1
+
+-- Treesitter
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "java", "python" },
+  sync_install = false,
+  auto_install = true,
+  -- ignore_install = { "javascript" },
+
+  highlight = {
+    enable = true,
+    -- disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
---
+++ config/nvim/plugin.lua
@@ -0,0 +1,64 @@
+-- Bootstrap packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	
+	-- Colorschemes
+	use 'dracula/vim'
+	use 'tomasr/molokai'
+	use 'morhetz/gruvbox'
+	use 'arcticicestudio/nord-vim'
+	-- use 'shaunsingh/nord.nvim'
+	
+	-- Eye Candy
+	-- use 'vim-buftabline'
+	-- use 'itchyny/lightline.vim'
+	use 'vim-airline/vim-airline'
+	use 'psliwka/vim-smoothie'
+	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+	-- use 'wellle/context.vim'
+	
+	-- Editing
+	use 'godlygeek/tabular'
+	use 'junegunn/vim-easy-align'
+	use '907th/vim-auto-save'
+	use 'tpope/vim-surround'
+	use 'tpope/vim-repeat'
+	use 'airblade/vim-gitgutter'
+	use 'tpope/vim-fugitive'
+	use {
+		'SirVer/ultisnips',
+		opt = true,
+		requires = {{'honza/vim-snippets', opt = true}}
+	}
+	use 'dkarter/bullets.vim'
+	use 'jiangmiao/auto-pairs'
+
+	-- Clipboard
+	use 'maxbrunsfeld/vim-yankstack'
+	use 'mbbill/undotree'
+	
+	-- Tmux
+	use 'christoomey/vim-tmux-navigator'
+
+	-- Buffers
+	use 'majutsushi/tagbar'
+	use 'unblevable/quick-scope'
+	use 'mhinz/vim-startify'
+	use 'scrooloose/nerdtree'
+end)

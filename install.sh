4120f4356e5d2921c3800db835b2ec921003a919
CookieUzen <uzen.huang@gmail.com>
2022-10-29 19:04:41 -0500

    hum1

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 # Change this variable if the folder is placed in another location
 # dotfiles="$HOME/dotfiles"
+
 dotfiles="."
+pwd > ~/dotfiles.log
 
 # Set up .config folder with symlink
 for folder in "$dotfiles"/config/*; do
         folder=$(basename $folder)
+	echo "Loading $folder" >> ~/dotfiles.log
+
         mkdir -p ~/.config/"$folder"
         ln -s "$dotfiles"/config/"$folder"/* ~/.config/"$folder"/
 done
